dd83b586d69e5eaa07b2f15b4bbf2810f81f11b4
Joey Gibson <joey@joeygibson.com>
2019-01-22 23:42:28 -0500

    chapter 26

---
+++ NugetFSharp/NugetFSharp/Library.fs
@@ -0,0 +1,16 @@
+﻿module NugetFSharp
+
+open Humanizer
+open Newtonsoft.Json
+
+type Person = { Name : string; Age : int }
+
+let getPerson () =
+    let text = """{"Name": "Sam", "Age": 23 }"""
+    let person = JsonConvert.DeserializeObject<Person>(text)
+    
+    printfn "Person called %s is %d years old." person.Name person.Age
+    
+    printfn "%s" ("ScriptsAreAGreatWayToExplorePackages".Humanize())
+    
+    person
